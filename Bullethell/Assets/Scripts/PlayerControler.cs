c663ae903a1fe119fb374477c272669d94b58ee3
Xengrick <xengrick99@gmail.com>
2024-04-16 01:53:04 -0700

    jugador agregado
    sprite del jugador y puede moverse

---
+++ Bullethell/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float velocidad = 5f;
+    private Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>(); // Obtén la referencia al Rigidbody2D
+    }
+
+    void Update()
+    {
+        // Resetea la velocidad para evitar acumulación de fuerzas
+        rb2D.velocity = Vector2.zero;
+
+        if (Input.GetKey("a"))
+        {
+            rb2D.AddForce(Vector2.left * velocidad, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb2D.AddForce(Vector2.right * velocidad, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb2D.AddForce(Vector2.up * velocidad, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb2D.AddForce(Vector2.down * velocidad, ForceMode2D.Impulse);
+        }
+    }
+}
