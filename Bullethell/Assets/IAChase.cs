6a90478ca56a1201ed492540f9680ceabf167096
Rogue199 <95267282+Rogue199@users.noreply.github.com>
2024-04-17 14:35:13 -0700

    reparación de bugs y añadir un nuevo enemigo

--- Bullethell/Assets/IAChase.cs
+++ Bullethell/Assets/IAChase.cs
@@ -18,10 +18,15 @@ public class IAChase : MonoBehaviour
     void Update()
     {
         distance = Vector2.Distance(transform.position,player.transform.position);
-        Vector2 = direction = player.transform.position - transform.position;
+        Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-        float angle = Math.Atan2(direction.y,direction.x) * Math.Rad2Deg;
-        transform.position = Vector2.MoveTowards(this,transform.position,player.transform.position, speed * Time.deltaTime);
+        float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
+       
+        if(distance < 4)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
 
+        }
     }
 }
