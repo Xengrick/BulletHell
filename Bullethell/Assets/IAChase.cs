3043afe519bee808a95cd169a2d6c9295ea3f476
Rogue199 <95267282+Rogue199@users.noreply.github.com>
2024-04-17 11:27:25 -0700

    se añadieron mas assets y codigo

---
+++ Bullethell/Assets/IAChase.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAChase : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    private float distance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position,player.transform.position);
+        Vector2 = direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Math.Atan2(direction.y,direction.x) * Math.Rad2Deg;
+        transform.position = Vector2.MoveTowards(this,transform.position,player.transform.position, speed * Time.deltaTime);
+
+    }
+}
