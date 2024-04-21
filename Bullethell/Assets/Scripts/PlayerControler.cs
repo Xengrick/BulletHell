c44d17a158dba3b6601e630d5644fb8608e229e0
Xengrick <xengrick99@gmail.com>
2024-04-20 20:14:28 -0700

    animación y cursor
    el cursor disparaba proyectiles y el personaje tiene animación al moverse

--- Bullethell/Assets/Scripts/PlayerControler.cs
+++ Bullethell/Assets/Scripts/PlayerControler.cs
@@ -6,7 +6,8 @@ public class PlayerControler : MonoBehaviour
 {
     public float velocidad = 5f;
     private Rigidbody2D rb2D;
- 
+    public GameObject projectilePrefab;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>(); // Obtén la referencia al Rigidbody2D
@@ -20,21 +21,40 @@ public class PlayerControler : MonoBehaviour
         if (Input.GetKey("a"))
         {
             rb2D.AddForce(Vector2.left * velocidad, ForceMode2D.Impulse);
+            gameObject.GetComponent<Animator>().SetBool("left", true);
         }
 
         if (Input.GetKey("d"))
         {
             rb2D.AddForce(Vector2.right * velocidad, ForceMode2D.Impulse);
+            gameObject.GetComponent<Animator>().SetBool("right", true);
         }
 
         if (Input.GetKey("w"))
         {
             rb2D.AddForce(Vector2.up * velocidad, ForceMode2D.Impulse);
+            gameObject.GetComponent<Animator>().SetBool("up", true);
         }
 
         if (Input.GetKey("s"))
         {
             rb2D.AddForce(Vector2.down * velocidad, ForceMode2D.Impulse);
+            gameObject.GetComponent<Animator>().SetBool("down", true);
+        }
+
+        if (Input.GetMouseButtonDown(0)) //mouse
+        {
+            //crea un projectil
+            Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        }
+
+        //animación
+        if(!Input.GetKey("w") && !Input.GetKey("s") && !Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("down", false);
+            gameObject.GetComponent<Animator>().SetBool("up", false);
+            gameObject.GetComponent<Animator>().SetBool("left", false);
+            gameObject.GetComponent<Animator>().SetBool("right", false);
         }
 
     }
