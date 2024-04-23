a84b00e3c90aee4e881ba1e510b347724b0deb8a
Rogue199 <95267282+Rogue199@users.noreply.github.com>
2024-04-23 07:18:34 -0700

    correción de errores

--- Bullethell/Assets/IAChase.cs
+++ Bullethell/Assets/IAChase.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class IAChase : MonoBehaviour
 {
-    public GameObject player;
+    public string playerTag = "Player"; 
     public float speed;
     private Rigidbody2D rb2D;
+    private GameObject player;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         rb2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+
+        player = GameObject.FindGameObjectWithTag(playerTag);
+
+        if (player == null)
+        {
+            Debug.LogError("No se encontró un objeto con la etiqueta 'Player'.");
+        }
     }
 
     void Update()
@@ -25,15 +34,13 @@ public class IAChase : MonoBehaviour
 
             difference.Normalize();
 
-            
+      
             if (Mathf.Abs(difference.x) > Mathf.Abs(difference.y))
             {
-               
                 rb2D.velocity = new Vector2(difference.x * speed, 0f);
             }
             else
             {
-                
                 rb2D.velocity = new Vector2(0f, difference.y * speed);
             }
         }
